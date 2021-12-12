ea9f4b35bdc80a9a8e14f388f48f30793ee31c09
Admin pc <dylanmwangidm765@gmail.com>
2021-12-12 14:59:04 +0300

    I had added a new intent to start the infoActivity

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -2,10 +2,13 @@ package com.pluralsight.candycoded;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
